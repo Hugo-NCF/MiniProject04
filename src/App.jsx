1c202127bf7167d1b6e09cc2328c7c0ad825e5e3
Raydar <d.beckford28@ncf.edu>
2026-02-20 15:04:23 -0500

    Add daisy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 export default function App() {
   return (
-    <div className="p-10">
-      <h1 className="text-4xl font-bold text-purple-600">
-        Tailwind is working 🚀
+    <div className="p-10 space-y-4">
+      <h1 className="text-3xl font-bold">
+        DaisyUI test
       </h1>
+
+      {/* DaisyUI button */}
+      <button className="btn btn-primary">
+        Primary Button
+      </button>
+
+      {/* DaisyUI alert */}
+      <div className="alert alert-success">
+        <span>DaisyUI is working 🌼</span>
+      </div>
+
+      {/* DaisyUI loader */}
+      <span className="loading loading-spinner loading-lg"></span>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import daisyui from 'daisyui'
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,8 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-    tailwindcss(),
+    tailwindcss({
+      plugins: [daisyui],
+    }),
   ],
 })
