1683354e576c6413643ab202db6ce1ea8f698b7c
Raydar <d.beckford28@ncf.edu>
2026-02-20 14:52:53 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-purple-600">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  )
+}
